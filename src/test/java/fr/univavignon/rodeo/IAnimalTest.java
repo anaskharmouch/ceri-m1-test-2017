8a2c00471976db22b185c7f76d72f288ee9c1cee
uapv1702620 <uapv1702620@e-mc137-121.univ-avignon.fr>
2018-04-18 14:18:45 +0200

    1st

---
+++ src/test/java/fr/univavignon/rodeo/IAnimalTest.java
@@ -0,0 +1,34 @@
+package fr.univavignon.rodeo;
+
+import fr.univavignon.rodeo.api.IAnimal;
+
+import org.junit.*;
+import org.mockito.*;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.mock;
+
+public class IAnimalTest {
+
+    IAnimal animal;
+   
+    @Before
+    public  void getInstance(){
+        animal=mock(IAnimal.class);
+    when(animal.isBoss()).thenReturn(false);
+    when(animal.getName()).thenReturn("Anas");
+    when(animal.getXP()).thenReturn(12);
+   
+
+    }
+   
+
+   
+    @Test
+    public void testBlabla(){
+    assertEquals(false,animal.isBoss());
+    System.out.println(animal.getXP());
+
+    }
+}
