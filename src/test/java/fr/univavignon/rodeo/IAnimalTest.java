7e79f707b005539a313f29f9acd41111d2f74ba3
uapv1702620 <uapv1702620@e-mc131-104.univ-avignon.fr>
2018-05-14 16:19:50 +0200

    key

--- src/test/java/fr/univavignon/rodeo/IAnimalTest.java
+++ src/test/java/fr/univavignon/rodeo/IAnimalTest.java
@@ -1,68 +1,52 @@
 package fr.univavignon.rodeo;
 
 
-
 import fr.univavignon.rodeo.api.IAnimal;
 
-import org.junit.*;
-import org.mockito.*;
+import static org.junit.Assert.assertEquals;
 
-import static org.junit.Assert.*;
-import static org.mockito.Mockito.when;
-import static org.mockito.Mockito.mock;
+import org.junit.Test;
+import org.mockito.Mockito;
 
-public class IAnimalTest {
 
-    IAnimal animal;
-   
-    @Before
-    public  void getInstance(){
-        animal=mock(IAnimal.class);
-    when(animal.isBoss()).thenReturn(false);
-    when(animal.getName()).thenReturn("chat");
-    when(animal.getXP()).thenReturn(8);
-    when(animal.isSecret()).thenReturn(true);
-    
-    when(animal.isEndangered()).thenReturn(false);
-    
-   
 
-    }
-   
- 
-   
-    @Test
-    public void testIsBoss(){
-    assertEquals(false,animal.isBoss());
-    System.out.println(animal.getXP());
+public class IAnimalTest  {
+	
+	public static IAnimal createMock(int xp, boolean secret, boolean endagered, boolean boss) {
+		IAnimal iAnimal = Mockito.mock(IAnimal.class);
+		Mockito.when(iAnimal.getXP()).thenReturn(xp);
+		Mockito.when(iAnimal.isSecret()).thenReturn(secret);
+		Mockito.when(iAnimal.isEndangered()).thenReturn(endagered);
+		Mockito.when(iAnimal.isBoss()).thenReturn(boss);
+		return iAnimal;
+}
+	public IAnimal getMock() {
+		return createMock(1899,true,false,true);
+		
+	}
+	
+		
 
-    }
-    
-    
-    @Test
-    public void testIsSecret(){
-    assertEquals(true,animal.isSecret());
-   
-
-    }
-    
-    @Test
-    public void testisEndangered(){
-    assertEquals(false,animal.isEndangered());
-   
-
-    }
-    @Test
-    public void testisgetName(){
-    assertEquals("chat",animal.getName());
-   
-
-    }
-    
-    @Test
-    public void testisgetXP(){
-    assertEquals(animal.getXP(),8);
-   
-
-    }
+	@Test
+	public void testgetXP(){
+		assertEquals(1899,createMock(1899,true,false,true).getXP());
+	}
+	
+	@Test
+	public void testIsBoss(){
+		assertEquals(true,createMock(1899,true,false,true).isBoss());
+	}
+	
+	
+	@Test
+	public void testIsEndangered(){
+		assertEquals(false,createMock(1899,true,false,true).isEndangered());
+	}
+	
+	@Test
+	public void testIsSecret(){
+		assertEquals(true,createMock(1899,true,false,true).isSecret());
+		
+	}
+	
 }
