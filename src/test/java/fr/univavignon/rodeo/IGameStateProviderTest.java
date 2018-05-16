18d1d55305a0d0256dd9b4b859304bd8e14d327b
uapv1702620 <uapv1702620@ge-c129-108.univ-avignon.fr>
2018-05-16 13:57:40 +0200

    Test IGameStateProvider

--- src/test/java/fr/univavignon/rodeo/IGameStateProviderTest.java
+++ src/test/java/fr/univavignon/rodeo/IGameStateProviderTest.java
@@ -1,14 +1,60 @@
 package fr.univavignon.rodeo;
 
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.doThrow;
 
+import java.util.ArrayList;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import fr.univavignon.rodeo.api.IAnimal;
+
+import fr.univavignon.rodeo.api.IGameState;
+import fr.univavignon.rodeo.api.IGameStateProvider;
+import fr.univavignon.rodeo.api.ISpecie;
 
 
 public class IGameStateProviderTest {
+			
+		static IGameStateProvider gameStateProvider;		
+		static IGameState gameState;	
+				
+		    public static  IGameStateProvider MockGameStateProvider(){		    	
+		    
+		        gameStateProvider=Mockito.mock(IGameStateProvider.class);		        
+		        Mockito.doThrow(new IllegalArgumentException()).when(gameStateProvider).get(null);
+		        gameState = IGameStateTest.MockGameState();			        
+		        Mockito.when(gameStateProvider.get("nothink")).thenReturn(gameState);        
+		        
+		        return gameStateProvider;		        
+		 }  
+		    
+		    
+		    @Before  
+		    public void initialisation(){
+		    	
+		    	gameStateProvider= MockGameStateProvider();
+		   	 
+		    }
+	
+		    
+		    
+		    @Test(expected=IllegalArgumentException.class)
+			public void testGetExcep(){
+				
+		    	gameStateProvider.get(null);
+				
+		    }
+	
+	
+		    @Test
+			public void testGet(){
+				
+				assertEquals(gameState, gameStateProvider.get("nothink"));
+				
+		    }
 
-	
-	
-	
-	
-	
-	
+
 }
